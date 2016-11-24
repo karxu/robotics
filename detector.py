ad1086017e88cab8afd2a4a8441224ea7b68a5e4
Onji <onjibae@gmail.com>
2016-11-24 13:24:16 +0100

    Detector.py Final

--- detector.py
+++ detector.py
@@ -22,10 +22,6 @@ c = ev3.ColorSensor(ev3.INPUT_3)
 c.connected
 c.mode = 'COL-REFLECT'
 
-###########################################################
-#################### Main Functions #######################
-###########################################################
-
 # "include if statement in the followLine code that calls the circumvent() function"
 # "when the sonar.value() < 150"
 
@@ -64,49 +60,3 @@ def circumvent():
             prev_val = sonar.value()
 
 # "you will now exit the if/else statement and go back to the beg of the while loop"
-
-###########################################################
-#################### Test Functions #######################
-###########################################################
-
-# use this to print sonar values at 3 sec intervals + store them in a .txt file
-def testSonar():
-
-    btn = ev3.Button()
-
-    readings = ""
-    readings_file = open('sonar_results.txt', 'w')
-
-    while not btn.backspace:
-        readings = readings + str(sonar.value()) + '\n'
-        print(str(sonar.value()))
-        time.sleep(3)
-    readings_file.write(readings)
-    readings_file.close()
-
-###########################################################
-################## Helper Functions #######################
-###########################################################
-
-# when turning right, motorR moves slightly as well so it can turn wider
-def turnR():
-    ev3.Sound.speak('Turning right').wait()
-    print("turn right")
-    motorL.run_timed(duty_cycle_sp = 50, time_sp=2000)
-    motorR.run_timed(duty_cycle_sp = 10, time_sp=2000)
-    time.sleep(.2)
-
-    time.sleep(.2)
-
-# when turning left, the eyes also turn so the robot can keep track of the wall
-def turnL():
-    print("turn left")
-    motorR.run_timed(duty_cycle_sp = 27, time_sp=2000)
-    motorM.run_timed(duty_cycle_sp = 45, time_sp=300)
-    time.sleep(.2)
-
-# moves forward
-def moveForward():
-    motorL.run_timed(duty_cycle_sp=25, time_sp=800)
-    motorR.run_timed(duty_cycle_sp=25, time_sp=800)
-    time.sleep(0.5)
